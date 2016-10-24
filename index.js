623b7cc3ac890e46dcebe94c25c7222ddda38e0e
Gabe <gafopi@gmail.com>
2016-10-23 23:35:38 -0400

    asdf

--- index.js
+++ index.js
@@ -16,13 +16,6 @@ var request = require("request");
 var receiveMessage = require('./lib/receive.js').receiveMessage;
 var fb = require('./lib/fb-messenger.js');
 
-
-var app = express();
-app.use(bodyParser.json({ verify: verifyRequestSignature }));
-//app.use(bodyParser.urlencoded({extended:false})); // to let us parse request body
-
-app.use(express.static(__dirname + '/www'));
-
 // LOAD CONFIGURATION
 var config;
 try {
@@ -30,9 +23,15 @@ try {
   console.log("Loaded configuration from " + process.argv[2]);
 } catch (Error) {
   console.log("Could not load configuration");
-	console.log('usage: node index.js <config_file>');
+  console.log('usage: node index.js <config_file>');
   process.exit(1);
 }
+
+var app = express();
+var app = require('./lib/fb-messenger')(app, config.facebook.app_secret, config.facebook.validation_token, config.facebook.page_access_token, config.facebook.server_url);
+app.use(express.static(__dirname + '/www'));
+//app.use(bodyParser.urlencoded({extended:false})); // to let us parse request body (this may mess up facebook parsing)
+
 
 // Wit.ai
 const accessToken = config.wit_ai.server_access_token;
@@ -56,127 +55,6 @@ app.post('/twilio_hook', function(req, res) {
   receiveMessage(client, twilio_client, body.From, body.Body, receiveMessage);
   res.send("");
 });
-
-
-// MESSENGER //////////////////////////////////////////////////////////////////
-
-/*
- * Use your own validation token. Check that the token used in the Webhook
- * setup is the same token used here.
- *
- */
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === config.facebook.validation_token) {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
-  }
-});
-
-/*
- * All callbacks for Messenger are POST-ed. They will be sent to the same
- * webhook. Be sure to subscribe your app to your page to receive callbacks
- * for your page.
- * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
- *
- */
-app.post('/webhook', function (req, res) {
-  var data = req.body;
-
-  // Make sure this is a page subscription
-  if (data.object == 'page') {
-    // Iterate over each entry
-    // There may be multiple if batched
-    data.entry.forEach(function(pageEntry) {
-      var pageID = pageEntry.id;
-      var timeOfEvent = pageEntry.time;
-
-      // Iterate over each messaging event
-      pageEntry.messaging.forEach(function(messagingEvent) {
-        if (messagingEvent.optin) {
-          fb.receivedAuthentication(messagingEvent);
-        } else if (messagingEvent.message) {
-          fb.receivedMessage(messagingEvent);
-        } else if (messagingEvent.delivery) {
-          fb.receivedDeliveryConfirmation(messagingEvent);
-        } else if (messagingEvent.postback) {
-          fb.receivedPostback(messagingEvent);
-        } else if (messagingEvent.read) {
-          fb.receivedMessageRead(messagingEvent);
-        } else if (messagingEvent.account_linking) {
-          fb.receivedAccountLink(messagingEvent);
-        } else {
-          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
-        }
-      });
-    });
-
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know you've
-    // successfully received the callback. Otherwise, the request will time out.
-    res.sendStatus(200);
-  }
-});
-
-/*
- * This path is used for account linking. The account linking call-to-action
- * (sendAccountLinking) is pointed to this URL.
- *
- */
-app.get('/authorize', function(req, res) {
-  var accountLinkingToken = req.query.account_linking_token;
-  var redirectURI = req.query.redirect_uri;
-
-  // Authorization Code should be generated per user by the developer. This will
-  // be passed to the Account Linking callback.
-  var authCode = "1234567890";
-
-  // Redirect users to this URI on successful login
-  var redirectURISuccess = redirectURI + "&authorization_code=" + authCode;
-
-  res.render('authorize', {
-    accountLinkingToken: accountLinkingToken,
-    redirectURI: redirectURI,
-    redirectURISuccess: redirectURISuccess
-  });
-});
-
-/*
- * Verify that the callback came from Facebook. Using the App Secret from
- * the App Dashboard, we can verify the signature that is sent with each
- * callback in the x-hub-signature field, located in the header.
- *
- * https://developers.facebook.com/docs/graph-api/webhooks#setup
- *
- */
-function verifyRequestSignature(req, res, buf) {
-  var signature = req.headers["x-hub-signature"];
-
-  if (!signature) {
-    // For testing, let's log an error. In production, you should throw an
-    // error.
-    console.error("Couldn't validate the signature.");
-  } else {
-    var elements = signature.split('=');
-    var method = elements[0];
-    var signatureHash = elements[1];
-
-    var expectedHash = crypto.createHmac('sha1', APP_SECRET)
-                        .update(buf)
-                        .digest('hex');
-
-    if (signatureHash != expectedHash) {
-      throw new Error("Couldn't validate the request signature.");
-    }
-  }
-}
-
-// END MESSENGER ///////////////////////////////////////////////////////////////
-
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/www/index.html');
