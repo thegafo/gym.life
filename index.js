fec5d48fe8e200014d02d344314c667b7c962e30
Gabe <gafopi@gmail.com>
2016-10-23 23:21:49 -0400

    verify request sig

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var fb = require('./lib/fb-messenger.js');
 
 
 var app = express();
-app.use(bodyParser.json({ verify: fb.verifyRequestSignature }));
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
 //app.use(bodyParser.urlencoded({extended:false})); // to let us parse request body
 
 app.use(express.static(__dirname + '/www'));
@@ -144,6 +144,36 @@ app.get('/authorize', function(req, res) {
     redirectURISuccess: redirectURISuccess
   });
 });
+
+/*
+ * Verify that the callback came from Facebook. Using the App Secret from
+ * the App Dashboard, we can verify the signature that is sent with each
+ * callback in the x-hub-signature field, located in the header.
+ *
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup
+ *
+ */
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+
+  if (!signature) {
+    // For testing, let's log an error. In production, you should throw an
+    // error.
+    console.error("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
+
+    var expectedHash = crypto.createHmac('sha1', APP_SECRET)
+                        .update(buf)
+                        .digest('hex');
+
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
 
 // END MESSENGER ///////////////////////////////////////////////////////////////
 
